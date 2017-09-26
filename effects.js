1553a7925adbf9126d2c9a12ce4fbc87136f6be3
Cormac Mollitor <cormacjmollitor@gmail.com>
2017-09-26 05:11:32 +0000

    Finished skills section and started work-experience

--- effects.js
+++ effects.js
@@ -53,7 +53,6 @@ function loadLiquidFillGauge(elementId, value, config) {
     var textPixels = (config.textSize*radius/2);
     var textFinalValue = parseFloat(value).toFixed(2);
     var textStartValue = config.valueCountUp?config.minValue:textFinalValue;
-    var percentText = config.displayPercent?"%":"";
     var circleThickness = config.circleThickness * radius;
     var circleFillGap = config.circleFillGap * radius;
     var fillCircleMargin = circleThickness + circleFillGap;
@@ -120,7 +119,7 @@ function loadLiquidFillGauge(elementId, value, config) {
 
     // Text where the wave does not overlap.
     var text1 = gaugeGroup.append("text")
-        .text(textRounder(textStartValue) + percentText)
+        .text(textRounder(textStartValue))
         .attr("class", "liquidFillGaugeText")
         .attr("text-anchor", "middle")
         .attr("font-size", textPixels + "px")
@@ -151,7 +150,7 @@ function loadLiquidFillGauge(elementId, value, config) {
 
     // Text where the wave does overlap.
     var text2 = fillCircleGroup.append("text")
-        .text(textRounder(textStartValue) + percentText)
+        .text(textRounder(textStartValue))
         .attr("class", "liquidFillGaugeText")
         .attr("text-anchor", "middle")
         .attr("font-size", textPixels + "px")
@@ -162,7 +161,7 @@ function loadLiquidFillGauge(elementId, value, config) {
     if(config.valueCountUp){
         var textTween = function(){
             var i = d3.interpolate(this.textContent, textFinalValue);
-            return function(t) { this.textContent = textRounder(i(t)) + percentText; }
+            return function(t) { this.textContent = textRounder(i(t)); }
         };
         text1.transition()
             .duration(config.waveRiseTime)
@@ -212,7 +211,7 @@ function loadLiquidFillGauge(elementId, value, config) {
 
             var textTween = function(){
                 var i = d3.interpolate(this.textContent, parseFloat(value).toFixed(2));
-                return function(t) { this.textContent = textRounderUpdater(i(t)) + percentText; }
+                return function(t) { this.textContent = textRounderUpdater(i(t)); }
             };
 
             text1.transition()
